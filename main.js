ff83c2ccbeb48ce50a96122be5349b926b9baefc
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-01-17 21:13:13 -0800

    figured it out

---
+++ main.js
@@ -0,0 +1,35 @@
+function madLib() {
+
+    var gotNoun1 = document.getElementById ('noun-one');
+    var gotNoun2 = document.getElementById ('noun-two');
+    var gotNoun3 = document.getElementById ('noun-three');
+    var gotNoun4 = document.getElementById ('noun-four');
+    var gotNoun5 = document.getElementById ('noun-five');
+    var gotVerb1 = document.getElementById ('verb-one');
+    var gotVerb2 = document.getElementById ('verb-two');
+    var gotadj = document.getElementById ('adj-one');
+    var gotProperNoun = document.getElementById ('proper-noun');
+    
+
+    //get values  by input
+    var displayNoun1 = gotNoun1.value;
+    var displayNoun2 = gotNoun2.value;
+    var displayNoun3 = gotNoun3.value;
+    var displayNoun4 = gotNoun4.value;
+    var displayNoun5 = gotNoun5.value;
+
+    var displayVerb1 = gotVerb1.value;
+    var displayVerb2 = gotVerb2.value;
+
+    var displayAdj = gotadj.value;
+
+    var displayProperNoun = gotProperNoun.value;
+
+    console.log(displayNoun1);
+    
+}
+
+//display
+
+
+
